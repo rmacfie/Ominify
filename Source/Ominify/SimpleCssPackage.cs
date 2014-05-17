8517c5659f17f4d4d5e8dc9d4caff10071a4ef44
rmacfie <robert@macfie.se>
2014-05-17 20:06:50 +0200

    Added simple CSS/JS packaging without minification (and without dependencies)

---
+++ Source/Ominify/SimpleCssPackage.cs
@@ -0,0 +1,24 @@
+﻿namespace Ominify
+{
+    public class SimpleCssPackage : OminifyPackage
+    {
+        public SimpleCssPackage(string packagePath) : base(packagePath)
+        {
+        }
+
+        protected override string Minify(string rawFileContent)
+        {
+            return rawFileContent;
+        }
+
+        public override string GetContentType()
+        {
+            return "text/css";
+        }
+
+        public override string GetHtmlElement(string url)
+        {
+            return string.Format("<link rel=\"stylesheet\" href=\"{0}\" />", url);
+        }
+    }
+}
---
+++ Source/Ominify/SimpleJsPackage.cs
@@ -0,0 +1,24 @@
+﻿namespace Ominify
+{
+    public class SimpleJsPackage : OminifyPackage
+    {
+        public SimpleJsPackage(string packagePath) : base(packagePath)
+        {
+        }
+
+        protected override string Minify(string rawFileContent)
+        {
+            return rawFileContent;
+        }
+
+        public override string GetContentType()
+        {
+            return "application/javascript";
+        }
+
+        public override string GetHtmlElement(string url)
+        {
+            return string.Format("<script src=\"{0}\"></script>", url);
+        }
+    }
+}
